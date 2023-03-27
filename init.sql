cd26280a9706a616491b8a3ef90e42b1cf023fe8
Yichen Yao <yy486@cornell.edu>
2023-03-27 17:08:37 -0400

    try 2 examples, again

--- init.sql
+++ init.sql
@@ -4,7 +4,6 @@ USE project;
 DROP TABLE IF EXISTS reviews;
 
 CREATE TABLE reviews(
-    id int DEFAULT NULL,
     professor varchar(255),
     overall int,
     difficulty int,
@@ -16,4 +15,5 @@ CREATE TABLE reviews(
 
 -- INSERT INTO reviews VALUE(1,'I''m Watching You','Bruce and Kris Jenner celebrate their anniversary. Kim and Tommy Davis buy themselves a stripper pole as a gift, and the youngest Jenner plays on it. Kim appears on The Tyra Banks Show Where she is interviewed about Kim''s sex tape. Kourtney deals with relationship drama.');
 
-INSERT INTO reviews VALUE(0,'Laura Barre',3,3,2,'I took this class with Roger Figueroa, Joanna Fiddler, and Laura Barre. The content of the class is really interesting and useful in everyday life. There are 5 easy in-class assignments, a quiz, and 3 exams. You don''t have to go to class and the workload is light, but exams are all multiple choice with really specific questions that you need to memorize off the slides.  ','NS 1150',0)
+INSERT INTO reviews VALUE('Laura Barre',3,3,2,'I took this class with Roger Figueroa, Joanna Fiddler, and Laura Barre. The content of the class is really interesting and useful in everyday life. There are 5 easy in-class assignments, a quiz, and 3 exams. You don''t have to go to class and the workload is light, but exams are all multiple choice with really specific questions that you need to memorize off the slides.  ','NS 1150',0);
+INSERT INTO reviews VALUE('John Michael',3,3,1,'The prelims were unnecessarily hard -- there would be the smallest details that would be asked. However, it was all T/F so i can''t really complain. the final exam was really easy though. There was not really much work and everything was basically in class. I would recommend the class if you are truly interested in nutrition.','NS 1150',0);
